f960f244760990c14756b71d52cc36d8c3905c49
John Flynn <jflynnpics@gmail.com>
2018-11-27 13:53:20 -0500

    done with explorer mode

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import step6 from './snowman/step_6.png'
 import step7 from './snowman/step_7.png'
 import './App.css'
 import Letter from './Letter'
+import word from './wordlist.json'
 
 class App extends Component {
   constructor(props) {
     super(props)
+    let randomIndex = Math.floor(Math.random() * 1024)
+    this.snowmen = [step0, step1, step2, step3, step4, step5, step6, step7]
     this.state = {
-      word: 'snowman',
+      word: word[randomIndex],
       revealedLetters: ['_', '_', '_', '_', '_', '_', '_'],
       pickableLetters: [
         'a',
@@ -44,22 +47,36 @@ class App extends Component {
         'y',
         'z'
       ],
-      pickedLetters: 0
+      backgroundImage: step0
     }
   }
-
+  snowman = () => {
+    let snowmanState = this.state.revealedLetters.filter(
+      letter => letter !== '_'
+    ).length
+    return this.snowmen[snowmanState]
+  }
   pickALetter = letter => {
     for (let index = 0; index < 7; index++) {
-      if ((this.state.word.split('')[index] = letter)) {
+      if (this.state.word.split('')[index] === letter) {
         this.state.revealedLetters.splice(index, 1, letter)
       }
     }
-  }
+    let remainingPickableLetters = this.state.pickableLetters
 
+    remainingPickableLetters.splice(
+      this.state.pickableLetters.indexOf(letter),
+      1
+    )
+
+    this.setState({
+      pickableLetters: remainingPickableLetters
+    })
+  }
   render() {
     return (
       <div className="App">
-        <img src={step0} className="snowman" alt="step" />
+        <img src={this.snowman()} className="snowman" alt="step" />
         <ul className="word">
           {this.state.revealedLetters.map(wordLetter => (
             <li>{wordLetter}</li>
@@ -67,7 +84,11 @@ class App extends Component {
         </ul>
         <ul className="letters">
           {this.state.pickableLetters.map(pickableLetter => (
-            <Letter pickALetter={this.pickALetter} value={pickableLetter} />
+            <Letter
+              key={pickableLetter}
+              pickALetter={this.pickALetter}
+              value={pickableLetter}
+            />
           ))}
         </ul>
         <h2>Pick a letter to build the Snowman!</h2>
